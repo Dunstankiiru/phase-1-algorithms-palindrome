db703e5e96027f6f5af5f9e763458e175b063ad4
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-03-21 20:10:30 +0300

    completes challenges

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-function isPalindrome(word) {
+function isPalindrome(string) {
   // Write your algorithm here
+  const reversedString = string.split("").reverse().join("");
+
+
+  return string === reversedString;
 }
 
 /* 
   Add your pseudocode here
+
+  1. Define function is palindrome that receives a string as argument
+
+  2. Reverse the string by converting string to array and reversing the array and changing it to string\
+
+  3. compare reversed string and original
+
+  4. If same return true and if not return false
+
+
 */
+
+//optimization
+function isPalindrome(string) {
+  let left = 0;
+  let right = string.length - 1;
+
+  while (left < right) {
+    if (string[left] !== string[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+
+
 
 /*
   Add written explanation of your solution here
